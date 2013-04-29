ac91b8f053ce59cf6f0e7d84f9b0d18324d2d530
Tim Sutton <tim@linfiniti.com>
2013-04-29 16:32:29 +0200

    Updated version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fabgis',
-    version='0.1.0',
+    version='0.2.0',
     author='Tim Sutton',
     author_email='tim@linfiniti.com',
     packages=['fabgis', ],
--- upload-to-pypi.sh
+++ upload-to-pypi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Have you updated changelog?"
+echo "Have you updated the version in setup.py?"
 echo "Have you updated README & Docs?"
 read -sn 1 -p "Press any key to continue..."
 python setup.py sdist upload
