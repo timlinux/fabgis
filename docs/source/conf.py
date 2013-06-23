bd2e600ecf294ce6a4b0a1cd4896c6947724ebd1
Tim Sutton <tim@linfiniti.com>
2013-06-23 12:12:21 +0700

    Fix version number in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ copyright = u'2013, Tim Sutton, Werner Macho'
 # built documents.
 #
 # The short X.Y version.
-version = '0'
+version = '0.8'
 # The full version, including alpha/beta/rc tags.
 release = '0.8'
 
