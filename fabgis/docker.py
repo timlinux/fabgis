328d735d1b63607ce990e9167eb7b6112f48ba0a
Tim Sutton <tim@linfiniti.com>
2013-08-25 01:06:59 +0200

    #3 Support for docker implementation (in progress)

--- fabgis/docker.py
+++ fabgis/docker.py
@@ -9,13 +9,48 @@ As the second article mentions, for ubuntu 12.04 the kernel needs to be
 upgraded to 3.8 - which the setup task will do.
 """
 
-from fabric.api import run, sudo, task, fastprint, env, prompt, reboot
+from fabric.api import run, sudo, task, fastprint, env, prompt, reboot, abort
 from fabric.colors import red, green, blue, yellow
-from fabtools.deb import update_index as apt_get_update
+from fabric.contrib.files import exists
+from fabtools.deb import update_index as apt_get_update, is_installed
 from fabtools.require.deb import ppa as require_ppa
 from fabtools.require.deb import package as require_package
 from fabtools.require.deb import packages as require_packages
-from fabtools.deb import is_installed
+
+
+@task
+def docker():
+    """Convenience function that operates in a similar way to fabtools vagrant.
+
+    Example usage - first add this to your fabfile.py
+
+        from fabgis.docker import docker
+
+    Then use docker instead of a -H hostname directive e.g.::
+
+        fab docker setup_qgis2
+
+    This command makes no attempt to create the docker container, so will fail
+    if there is not a fabgis.container.id file in the current working
+    directory that contains the id of a valid container.
+
+
+    .. todo:: Behaviour when more than one host is passed to fabric is still
+        not defined or tested.
+    """
+    container_id_file = 'fabgis.container.id'
+    if exists(container_id_file):
+        container_id = run('cat %s' % container_id_file)
+    else:
+        abort(red('Docker fabgis.container.id file not found.\n'))
+
+    mappings = get_docker_port_mappings(container_id)
+
+    ssh_port = mappings[22]
+
+    env.port = ssh_port
+    env.user = 'root'
+    fastprint(env)
 
 
 @task
@@ -51,19 +86,140 @@ def setup_docker():
     require_packages([
         'software-properties-common',
         'lxc-docker'])
-    start_docker_daemon()
-    run('docker pull base')
-    fastprint(green('Installation of oracle java completed ok!'))
+    setup_docker_image()
 
 
 @task
-def start_docker_daemon():
-    """Start the docker daemon."""
-    fastprint(yellow('Starting docker on host: %s' % env.host))
-    sudo('docker -d &')
+def setup_docker_image():
+    """Set up the default docker image to be used in fabgis deployments.
+
+    We base our container on dhrp/sshd image.
+
+    After running this task you will have an image called fabgis/sshd with
+    a root password of your choosing.
+
+    On first installation we need to set the root password that will be
+    used as default for this base image.
+    """
+
+    fastprint(blue(
+        'Downloading the dhrp/sshd base image - may take a while.\n'))
+
+    container_id = create_docker_container('dhrp/sshd')
+
+    fastprint(blue(
+        'setting the password in the container\n'))
+    fastprint(green(
+        'Enter the password "screencast" for the first root prompt below.\n'))
+    fastprint(green(
+        'Enter a password of YOUR OWN CHOOSING at the second prompt below.\n'))
+    fastprint(green(
+        'Then re-enter your own password at the confirmation prompt.\n'))
+    fastprint(red(
+        'Note that the password will be echoed to the console in the clear'
+        '.\n'))
+
+    mappings = get_docker_port_mappings(container_id)
+    run('ssh root@localhost -p %i passwd' % mappings[22])
+
+    fastprint(green(
+        'Committing the change to a new default container fabgis/sshd\n'))
+
+    sudo('sudo docker commit %s fabgis/sshd' % container_id)
+
+    fastprint(red(
+        'Deleting the temporary container we started during setup.\n'))
+
+    sudo('docker kill %s' % container_id)
+
+    fastprint(green('Installation of docker completed ok!\n'))
 
 
 @task
-def create_docker_container():
-    """Create a docker container."""
-    run('docker run -i -t base /bin/bash')
+def get_docker_port_mappings(container_id):
+    """Given the id of a container, get the ports mapped for that container.
+
+    This is basically a wrapper for docker ps that parses the output and
+    returns the ports that are mapped as a dict where the keys are the
+    container internal ports and the values are the container external ports.
+
+    :param container_id: ID for the container to obtain port mappings for.
+    :type container_id: str
+
+    e.g. for this container::
+
+        d3caf337bfc1     dhrp/sshd:latest    /usr/sbin/sshd -D   8 minutes \
+        ago       Up 8 minutes        49171->22, 49172->8000
+
+    The following dictionary would be returned::
+
+        {
+            22: 49171,
+            8000, 49172
+        }
+    """
+    ports = sudo(
+        'docker ps | grep %s | awk '
+        '\'{ s = ""; for (i = 11; i <= NF; i++) s = s $i " "; print s }\''
+        % container_id)
+
+    #fastprint('Ports: %s\n' % str(ports))
+
+    if ', ' in ports:
+        tokens = ports.split(', ')
+    else:
+        tokens = [ports]
+
+    mappings = {}
+
+    #fastprint('Tokens: %s\n' % str(tokens))
+    for token in tokens:
+        parts = token.split('->')
+        #fastprint('Parts: %s\n' % str(parts))
+        host_port = int(parts[0])
+        container_port = int(parts[1])
+        mappings[container_port] = host_port
+
+    fastprint('Mappings: %s\n' % str(mappings))
+    return mappings
+
+
+@task
+def create_docker_container(image='fabgis/sshd'):
+    """Create a docker container using the provided base image.
+
+    :param image: The image name to be used as a basis for this container.
+    :type image: str
+
+    :returns: The id of the created container.
+    :rtype: str
+    """
+    # Id of container will be written to fabgis.container.id
+
+    container_id_file = 'fabgis.container.id'
+    if exists(container_id_file):
+        sudo('rm %s' % container_id_file)
+
+    sudo(
+        'docker run -cidfile=%s -d -p 22 -p 8000 %s /usr/sbin/sshd -D' %
+        (container_id_file, image))
+
+    # now get the id back - this is more reliable than reading from stdout
+    container_id = run('cat %s' % container_id_file)
+
+    return container_id
+
+
+@task
+def current_docker_container():
+    """Return the current docker container id as stored in fabgis.container.id.
+
+    :returns: The value in fabgis.container.id. If the container does not exist
+        None is returned.
+    :rtype: str, None
+    """
+    container_id_file = 'fabgis.container.id'
+    if exists(container_id_file):
+        return run('cat %s' % container_id_file)
+    else:
+        return None
