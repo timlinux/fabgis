9b0542f6711f412dfee0765723a3bf6ef69ae3ef
Tim Sutton <tim@linfiniti.com>
2013-08-30 10:03:17 +0200

    Added placeholder for installation of cartodb

---
+++ fabgis/cartodb.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""Tools for deployment of cartodb on a server.
+
+Note this is currently just a place holder. I plan to port these scripts to
+fabric scripts:
+
+https://github.com/nighthacker/cartodb-dev
+
+Tim Sutton, August 2013"""
