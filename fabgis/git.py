f3947d47f1a270126b02c428e1742c521db6e32d
Tim Sutton <tim@linfiniti.com>
2013-09-27 13:18:57 +0200

    Tweaks to git checkout task and added a task to strip local git repo of branches

--- fabgis/git.py
+++ fabgis/git.py
@@ -1,13 +1,13 @@
 """Git related utilities."""
 import os
-from fabric.api import fastprint, run, cd
-from fabric.colors import red, blue, green
+from fabric.api import fastprint, run, cd, task, env
+from fabric.colors import red, cyan, green
 from fabric.contrib.files import exists
 import fabtools
 
 from .common import setup_env
 
-
+@task
 def update_git_checkout(code_path, url, repo_alias, branch='master', tag=None):
     """Make sure there is a read only git checkout.
 
@@ -32,19 +32,19 @@ def update_git_checkout(code_path, url, repo_alias, branch='master', tag=None):
         fab -H 188.40.123.80:8697 remote update_git_checkout
 
     """
-    fastprint(blue('Updating git checkout.'))
-    fastprint(blue('code_path: %s' % code_path))
-    fastprint(blue('url: %s' % url))
-    fastprint(blue('repo_alias: %s' % repo_alias))
-    fastprint(blue('branch: %s' % branch))
-    fastprint(blue('tag: %s' % tag))
+    fastprint(cyan('Updating git checkout.\n'))
+    fastprint(cyan('code_path: %s\n' % code_path))
+    fastprint(cyan('url: %s\n' % url))
+    fastprint(cyan('repo_alias: %s\n' % repo_alias))
+    fastprint(cyan('branch: %s\n' % branch))
+    fastprint(cyan('tag: %s\n' % tag))
     setup_env()
     repo_path = os.path.join(code_path, repo_alias)
 
     fabtools.require.deb.package('git')
     if not exists(repo_path):
-        fastprint(green('Basic Repository does not exist, creating.'))
-        run('mkdir -p %s' % code_path)
+        fastprint(green('Repository does not exist, creating.\n'))
+        fabtools.require.directory(code_path, use_sudo=True, env.user)
         with cd(code_path):
             run('git clone %s %s' % (url, repo_alias))
     else:
@@ -74,3 +74,25 @@ def update_git_checkout(code_path, url, repo_alias, branch='master', tag=None):
                 run('git checkout master')
             run('git pull')
     fastprint(green('Checking out tag completed.'))
+
+
+@task
+def remove_local_branches(code_path):
+    """Remove any local branches you may have in your repo - use with caution!
+
+    Any local changes will be dropped. Master will be checked out and all
+    other local branches removed.
+
+    :param code_path: Path to where the repo should be.
+    :type code_path: str
+
+    .. versionadded:: 0.16.0
+    """
+    fastprint(red('Removing all local branches from repo! %s\n' % code_path))
+    with cd(code_path):
+        # Get rid of any local changes
+        run('git reset --hard')
+        # Get back onto master branch
+        run('git checkout master')
+        # Delete all local branches
+        run('git branch | grep -v \* | xargs git branch -D')
